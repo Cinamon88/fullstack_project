28edd20c07149b82b4ead682068835199e145c85
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-03-14 12:24:58 +0100

    add routes, path to index and message Not found if 404

--- server.js
+++ server.js
@@ -5,26 +5,35 @@ const connectToDB = require('./db');
 
 
 // start express server
-
 const app = express();
-
 app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
 
-
 // connect to DB
-
 connectToDB();
 
-
 // add middleware
-
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
 // serve static files from React App
+app.use(express.static(path.join(__dirname, '/client/build')));
 
-app.use(express.static(path.join(__dirname, '/client/build')))
+// add routes
+app.use('/api', require('./routes/ads.routes'));
+app.use('/api', require('./routes/users.routes'));
+app.use('/auth', require('./routes/auth.routes'));
+
+// at any other link just serve React App
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'Not found...' });
+});
+
+
 
