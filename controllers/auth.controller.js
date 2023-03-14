a8cdb51666bffcfb6c2544db4a37a82d8bd4b886
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-03-14 20:46:32 +0100

    end func by return

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const user = require('../models/user.model');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 
 exports.register = async (req, res) => {
     try {
@@ -7,12 +7,16 @@ exports.register = async (req, res) => {
         if (login && typeof login === 'string' && password && typeof password === 'string') {
             const userWithLogin = await user.findOne({ login });
             if (userWithLogin) {
-                res.status(409).send({ message: 'User with this login already exists'});
+                return res.status(409).send({ message: 'User with this login already exists'});
             }   
             
             const user = await user.create({ login, password: await bcrypt.hash( password, 10) });
             res.status(201).send({ message: 'User created!' + user.login });
+
+        } else {
+            res.status(400).send({ message: 'Bad request' });
         }
+
     } catch (err) {
         res.status(500).send({ message: err.message });
     }
