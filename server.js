5040ac1b4f4ff227ae693a96f13416638cbf0caa
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-03-15 16:40:59 +0100

    add cookie & update cors

--- server.js
+++ server.js
@@ -16,11 +16,27 @@ app.listen(process.env.PORT || 8000, () => {
 // connect to DB
 connectToDB();
 
+
 // add middleware
-app.use(cors());
+if(process.env.NODE_ENV !== 'production') {
+  app.use(
+    cors({
+      origin: ['http://localhost:3000'],
+      credentials: true,
+    })
+  );
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(session({ secret: 'xyz567', store: MongoStore.create(mongoose.connection), resave: false, saveUninitialized: false }));
+app.use(session({ 
+  secret: 'xyz567', 
+  store: MongoStore.create(mongoose.connection), 
+  resave: false, 
+  saveUninitialized: false,
+  cookie: {
+    secure: process.env.NODE_ENV == 'production',
+  },
+}));
 
 // serve static files from React App
 app.use(express.static(path.join(__dirname, '/client/build')));
