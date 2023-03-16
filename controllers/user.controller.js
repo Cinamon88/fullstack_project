f899e087f6fbb169e44a9644b1d4d1097db1573b
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-03-16 12:41:14 +0100

    add user routes & controller

---
+++ controllers/user.controller.js
@@ -0,0 +1,13 @@
+const User = require('../models/User.model');
+
+exports.register = async (req, res) => {
+
+};
+
+exports.login = async (req, res) => {
+
+};
+
+exports.users = async (req, res) => {
+
+};
