a79fc12a91648ba18cd106cc437a6e1b7fd2cf07
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-03-18 14:44:23 +0100

    synchronize form with server

--- client/src/components/pages/Register/Register.js
+++ client/src/components/pages/Register/Register.js
@@ -1,29 +1,40 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
 const Register = () => {
+
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [phone, setPhone] = useState('');
+    const [avatar, setAvatar] = useState(null);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+    };
+
     return (
         <Form className="col-12 col-sm-3 mx-auto">
             <h1 className='my-4'>Sign up</h1>
 
-            <Form.Group className='mb-3' controlId='formLogin'>
+            <Form.Group className='mb-3' controlId='formLogin' onSumbit={handleSubmit}>
                 <Form.Label>Login</Form.Label>
-                <Form.Control type="text" placeholder="Enter login" />
+                <Form.Control type="text" value={login} onChange={e => setLogin(e.target.value)} placeholder="Enter login" />
             </Form.Group>
 
             <Form.Group className='mb-3' controlId='formPassword'>
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" />
             </Form.Group>
 
             <Form.Group className='mb-3' controlId='formPhone'>
                 <Form.Label>Phone numebr</Form.Label>
-                <Form.Control type="tel" placeholder="Phone number" />
+                <Form.Control type="tel" value={phone} onChange={e => setPhone(e.target.value)} placeholder="Phone number" />
             </Form.Group>
 
             <Form.Group className='mb-3' controlId='formFile'>
                 <Form.Label>Avatar</Form.Label>
-                <Form.Control type="file" />
+                <Form.Control type="file" onChange={e => setAvatar(e.target.files[0])} />
             </Form.Group>
 
             <Button variant="primary" type="submit">
