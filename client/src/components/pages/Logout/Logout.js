aa86f2728a597fae6dc2d7068d702f3a16bf243b
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-03-18 20:00:51 +0100

    use logOut method in Logout component

---
+++ client/src/components/pages/Logout/Logout.js
@@ -0,0 +1,23 @@
+import { API_URL } from "../../../config";
+import { logOut } from "../../../redux/usersRedux";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+
+const Logout = () => {
+    
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+    const options = {
+        method: 'DELETE',
+    };
+
+    fetch(`${API_URL}/logout`, options)
+        .then(res => {
+            dispatch(logOut());
+        })
+    }, [dispatch]);
+    return null;
+};
+
+export default Logout;
