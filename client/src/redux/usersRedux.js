c80f1aca17fb69a28c93f2fb628d5ce5ca19c620
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-03-18 19:58:59 +0100

    add new action LOG_OUT

--- client/src/redux/usersRedux.js
+++ client/src/redux/usersRedux.js
@@ -1,6 +1,7 @@
 // actions
 const createActionName = (actionName) => `app/users/${actionName}`;
 const LOG_IN = createActionName('LOG_IN');
+const LOG_OUT = createActionName('LOG_IN');
 
 // action creators
 export const logIn = payload => ({
@@ -8,11 +9,17 @@ export const logIn = payload => ({
     payload
 });
 
+export const logOut = () => ({
+    type: LOG_OUT,
+});
+
 
 const usersReducer = (statePart = null, action) => {
     switch (action.type) {
         case LOG_IN:
             return action.payload;
+        case LOG_OUT:
+            return null
         default:
             return statePart;
     }
