afd8d1810e89a9288abf195cff7b3cf78612f7ba
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-03-18 14:28:03 +0100

    add register form

--- client/src/components/pages/Register/Register.js
+++ client/src/components/pages/Register/Register.js
@@ -1,6 +1,36 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 const Register = () => {
     return (
-        <div>Register</div>
+        <Form className="col-12 col-sm-3 mx-auto">
+            <h1 className='my-4'>Sign up</h1>
+
+            <Form.Group className='mb-3' controlId='formLogin'>
+                <Form.Label>Login</Form.Label>
+                <Form.Control type="text" placeholder="Enter login" />
+            </Form.Group>
+
+            <Form.Group className='mb-3' controlId='formPassword'>
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group className='mb-3' controlId='formPhone'>
+                <Form.Label>Phone numebr</Form.Label>
+                <Form.Control type="tel" placeholder="Phone number" />
+            </Form.Group>
+
+            <Form.Group className='mb-3' controlId='formFile'>
+                <Form.Label>Avatar</Form.Label>
+                <Form.Control type="file" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+            
+        </Form>
     );
 };
 
