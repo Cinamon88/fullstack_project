f387ff2f0a84af0cab48231417b89c8aa8bcdbf3
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-03-21 13:08:03 +0100

    modify nav

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <Navbar bg="primary" variant="dark" expand="lg" className="mt-4 mb-4 rounded d-flex justify-content-between">
+        <Navbar bg="success" variant="dark" expand="lg" className="mt-4 mb-4 rounded d-flex justify-content-between">
             <NavbarBrand className="justify-content-start px-3" href="/">Ads.app</NavbarBrand>
                 <Nav className="flex-sm-column flex-md-row px-3">
                     <Nav.Link className="px-1" to="/" as={NavLink}>
