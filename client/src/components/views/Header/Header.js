e1aa566839a6c7287c650535cc4ee2c8681cab96
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-03-22 17:55:39 +0100

    add header & header style

--- client/src/components/views/Header/Header.js
+++ client/src/components/views/Header/Header.js
@@ -1,9 +1,67 @@
-import NavBar from '../NavBar/NavBar';
+import styles from './Header.module.scss';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
+    const user = 1;
+
     return (
-        <NavBar />
+        <nav>
+            <div className={styles.nav_wrapper}>
+                <NavLink to="/" className={styles.logo}>
+                    <h2>Ads App</h2>
+                </NavLink>
+                <ul className={styles.nav_list}>
+                    <li>
+                        <NavLink
+                            className={({ isActive }) =>
+                                isActive ? styles.linkActive : undefined
+                            }
+                            to="/"
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        {!user && (
+                            <NavLink
+                                className={({ isActive }) =>
+                                    isActive ? styles.linkActive : undefined
+                                }
+                                to="/login"
+                            >
+                                Sign in
+                            </NavLink>
+                        )}
+                    </li>
+                    <li>
+                        {!user && (
+                            <NavLink
+                                className={({ isActive }) =>
+                                    isActive ? styles.linkActive : undefined
+                                }
+                                to="/register"
+                            >
+                                Register
+                            </NavLink>
+                        )}
+                    </li>
+                    <li>
+                        {user && (
+                            <NavLink
+                                className={({ isActive }) =>
+                                    isActive ? styles.linkActive : undefined
+                                }
+                                to="/logout"
+                            >
+                                Logout
+                            </NavLink>
+                        )}
+                    </li>
+                </ul>
+            </div>
+        </nav>
     );
 };
+
 
 export default Header;
