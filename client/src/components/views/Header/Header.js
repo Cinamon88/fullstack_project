d5d129c0be27cc5ee6626bf068001dbc6bf9e110
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-03-23 15:59:37 +0100

    use NavBar in Header

--- client/src/components/views/Header/Header.js
+++ client/src/components/views/Header/Header.js
@@ -1,67 +1,9 @@
-import styles from './Header.module.scss';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux'
-import { getUser } from '../../../redux/usersRedux';
+import NavBar from '../NavBar/NavBar';
 
 const Header = () => {
-    const user = useSelector(getUser)
 
     return (
-        <nav>
-            <div className={styles.nav_wrapper}>
-                <NavLink to="/" className={styles.logo}>
-                    <h2>Ads App</h2>
-                </NavLink>
-                <ul className={styles.nav_list}>
-                    <li>
-                        <NavLink
-                            className={({ isActive }) =>
-                                isActive ? styles.linkActive : undefined
-                            }
-                            to="/"
-                        >
-                            Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        {!user && (
-                            <NavLink
-                                className={({ isActive }) =>
-                                    isActive ? styles.linkActive : undefined
-                                }
-                                to="/login"
-                            >
-                                Sign in
-                            </NavLink>
-                        )}
-                    </li>
-                    <li>
-                        {!user && (
-                            <NavLink
-                                className={({ isActive }) =>
-                                    isActive ? styles.linkActive : undefined
-                                }
-                                to="/register"
-                            >
-                                Register
-                            </NavLink>
-                        )}
-                    </li>
-                    <li>
-                        {user && (
-                            <NavLink
-                                className={({ isActive }) =>
-                                    isActive ? styles.linkActive : undefined
-                                }
-                                to="/logout"
-                            >
-                                Logout
-                            </NavLink>
-                        )}
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <NavBar />
     );
 };
 
