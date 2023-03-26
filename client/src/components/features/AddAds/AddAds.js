098781c93f40716cf227329d7176297a077cc6b6
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-03-26 20:14:51 +0200

    change fetch path

--- client/src/components/features/AddAds/AddAds.js
+++ client/src/components/features/AddAds/AddAds.js
@@ -3,17 +3,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { API_URL } from '../../../config';
 import { addAd, updateAds } from '../../../redux/adsRedux';
-import { getUser } from '../../../redux/usersRedux';
 import AdForm from '../AdForm/AdForm';
+import { getUserId } from '../../../redux/userData';
 
 const AddAds = () => {
-  const user = useSelector(getUser);
-  console.log(user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const userId = useSelector(getUserId);
 
   const handleSubmit = (ad) => {
-    console.log(ad);
     const fd = new FormData();
     fd.append('title', ad.title);
     fd.append('description', ad.description);
@@ -22,23 +20,24 @@ const AddAds = () => {
     fd.append('localization', ad.localization);
     fd.append('phoneNumber', ad.phoneNumber);
     fd.append('image', ad.image);
-    fd.append('user', user.login);
+    fd.append('user', ad.user);
+
 
     const options = {
       method: 'POST',
       body: fd,
       credentials: 'include',
     };
-    fetch(API_URL + `/api/ads`, options).then((res) => {
+    fetch(`${API_URL}/ads`, options).then((res) => {
       if (res.status === 200) {
         dispatch(addAd(ad));
-        dispatch(updateAds());
-        navigate('/');
+        dispatch(updateAds())
+        navigate('/')
       }
     });
   };
 
-  return <AdForm action={handleSubmit} actionText="Add" />;
+  return <AdForm action={handleSubmit} actionText="Add" userId={userId} />;
 };
 
 export default AddAds;
