0d29033288daa1efc4b7199d08ed0fe8a26a2239
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-03-26 17:14:14 +0200

    add Ad page

--- client/src/components/pages/Ad/Ad.js
+++ client/src/components/pages/Ad/Ad.js
@@ -1,43 +1,94 @@
-import React from 'react';
-import styles from './Ad.module.scss';
-import Card from 'react-bootstrap/Card';
+import React, { useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { API_URL, IMGS_URL } from '../../../config';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getAdById, updateAds } from '../../../redux/adsRedux';
+import styles from './Ad.module.scss';
 import { Link } from 'react-router-dom';
-import { IMGS_URL } from '../../../config';
+import AdDelete from '../../features/AdDelete/AdDelete';
+import { getUser } from '../../../redux/usersRedux';
+import { getUserId } from '../../../redux/userData';
 
-const Ad = ({ title, price, localization, image, _id, user }) => {
-    return (
-      <Card className={styles.card_wrapper}>
-        <Card.Img
-          variant="top"
-          src={IMGS_URL + image}
-          className={styles.card_image}
-        />
-        <Card.Body>
-          <div className={styles.body}>
-            <Card.Title>Price: {price}£</Card.Title>
-            <Card.Subtitle className="my-3">
-              <b>{title}</b>
-            </Card.Subtitle>
-            <Card.Text className="mb-3">
-              <b>Location: {localization}</b>
-            </Card.Text>
-          </div>
-          <Row>
-            <Col>
-              <Link to={'/ads/' + _id}>
-                <Button className={styles.button} variant="primary">
-                  Read more
+const Ad = () => {
+  const navigate = useNavigate();
+  const user = useSelector(getUser);
+
+  const adId = useParams();
+  const id = adId.id;
+  const adData = useSelector((state) => getAdById(state, id));
+  const userId = useSelector(getUserId);
+
+  const [showAd, setShowAd] = useState(false);
+  const handleClose = () => setShowAd(false);
+  const handleShow = () => setShowAd(true);
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    const options = {
+      method: 'DELETE',
+      credentials: 'include',
+    };
+    fetch(API_URL + '/api/ads/' + id, options);
+    updateAds();
+    navigate('/');
+  };
+  return (
+    <div>
+      <Row className="d-flex justify-content-center mt-5">
+        {showAd && (
+          <AdDelete
+            showAd={showAd}
+            handleClose={handleClose}
+            handleDelete={handleDelete}
+          />
+        )}
+        ;
+        <Col xs="12" lg="5">
+          <Card className={styles.card_wrapper}>
+            <Card.Img variant="top" src={IMGS_URL + adData.image} />
+
+            <Card.Body>
+              <Card.Title className="mb-3">Price: {adData.price}$</Card.Title>
+              <Card.Subtitle className="mb-3">
+                <b>Title: {adData.title}</b>
+              </Card.Subtitle>
+              <Card.Text className="mb-3">
+                <b>Localization: {adData.localization}</b>
+              </Card.Text>
+              <Card.Text>{adData.description}</Card.Text>
+              <Card.Text>Published: {adData.date}</Card.Text>
+              <Card.Text>Author: {adData.user.login}</Card.Text>
+              <Card.Text>
+                Avatar:{' '}
+                <img
+                  className={styles.avatar}
+                  src={IMGS_URL + adData.user.avatar}
+                  alt="user avatar"
+                />
+              </Card.Text>
+              <Card.Text>Phone number: {adData.user.phoneNumber}</Card.Text>
+            </Card.Body>
+            {userId === adData.user._id && (
+              <Col className={styles.button} xs="12" lg="4">
+                <Link to={'/ads/edit/' + id}>
+                  <Button variant="outline-info" className="m-2">
+                    Edit
+                  </Button>
+                </Link>
+                <Button variant="outline-danger" onClick={handleShow}>
+                  Delete
                 </Button>
-              </Link>
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
-    );
+              </Col>
+            )}
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
 };
-
 
 export default Ad;
